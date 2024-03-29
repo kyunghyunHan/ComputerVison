24e415377f4b20a72b235900bdd9ebb952079f26
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-29 14:59:51 +0900

    feat:yolo_custom

--- py/custom_yolo/object_detection.py
+++ py/custom_yolo/object_detection.py
@@ -18,37 +18,35 @@ if deterministic:
 	torch.backends.cudnn.deterministic = True
 	torch.backends.cudnn.benchmark = False
 
-names=open('./yolo_data/obj.names','r', encoding='UTF8')
-Names=names.readlines()
-for i in Names:
-  print(i)
+
 
 np.random.seed(724)
-dir_main = "./yolo_data/"
-filenames_image = glob(f"{dir_main}/train/images/*.jpeg")
-filenames_label = [filename.replace('images', 'labels').replace('jpeg', 'txt') for filename in filenames_image]
+# dir_main = "./yolo_data/"
+# filenames_image = glob(f"{dir_main}/train/images/*.jpeg")
+# filenames_label = [filename.replace('images', 'labels').replace('jpeg', 'txt') for filename in filenames_image]
 
 
-print(filenames_label)
+# print(filenames_label)
 
 
-classes = ["dog", "cat"]
+# classes = ["dog", "cat"]
 
-import yaml
+# import yaml
 
-data = {
-    "path": './yolo_data/', # dataset root dir
-    "train" : 'train/images',
-    "val" : 'valid/images',
-    "names" : {0 : 'dog', 1 : 'cat'}}
+# data = {
+#     "path": './american/', # dataset root dir
+#     "train" : 'train/images',
+#     "val" : 'valid/images',
+#     "names" : {0 : 'dog', 1 : 'cat'}}
 
-with open('./tld.yaml', 'w') as f :
-    yaml.dump(data, f)
+
+# with open('./tld.yaml', 'w') as f :
+#     yaml.dump(data, f)
 
 # check written file
-with open('./tld.yaml', 'r') as f :
-    lines = yaml.safe_load(f)
-    print(lines)
+# with open('./tld.yaml', 'r') as f :
+#     lines = yaml.safe_load(f)
+#     print(lines)
 
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO('./yolov8n.pt')
