39f1e1da5da4403db8e62ce6c1012643eb1187c6
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-08-12 13:23:26 +0900

    feat:streo

--- window_display.py
+++ window_display.py
@@ -5,5 +5,7 @@ img = cv.imread("./img/face1.jpeg")
 if img is None:
     sys.exit("파일을 찾을수 없습니다.")
 cv.imshow("Image Display",img) #윈도우에 영상표시
+
+#waitKey(10000) => 10초 대기 키입력이 안일어나면 -1 반환
 cv.waitKey()
 cv.destroyAllWindows()
